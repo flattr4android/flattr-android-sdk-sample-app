2e36596cc087bd88ca6f32e000e4d49c8aa82409
Philippe Bernard <philippe.bernard@gmail.com>
2012-05-08 11:52:49 +0200

    Project refactored to use the SDK as an Android library

--- src/com/flattr4android/example/sdk/MainActivity.java
+++ src/com/flattr4android/example/sdk/MainActivity.java
@@ -12,19 +12,12 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		if ((getString(R.string.token).length() < 10)
-				|| (getString(R.string.token_secret).length() < 10)) {
-			setContentView(R.layout.setup);
-		} else {
-			setContentView(R.layout.main);
+		setContentView(R.layout.main);
 
-			LinearLayout ll = (LinearLayout) findViewById(R.id.button_prog_layout);
-			FlattrButton button = new FlattrButton(this);
-			button.setFlattrCredentials(getString(R.string.token),
-					getString(R.string.token_secret));
-			button.setThingId(getString(R.string.sample_thing_id));
-			button.setButtonStyle(FlattrButton.BUTTON_STYLE_HORIZONTAL);
-			ll.addView(button);
-		}
+		LinearLayout ll = (LinearLayout) findViewById(R.id.button_prog_layout);
+		FlattrButton button = new FlattrButton(this);
+		button.setThingId(getString(R.string.sample_thing_id));
+		button.setButtonStyle(FlattrButton.BUTTON_STYLE_HORIZONTAL);
+		ll.addView(button);
 	}
 }
